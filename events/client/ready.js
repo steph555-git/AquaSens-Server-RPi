4d41e0335cd0e5e7875934b39ab171c267f682f8
Stephane <horlogerie@alabonneheure.fr>
2022-11-03 00:53:23 +0200

    03/11/22 Interaction avec bot

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,11 @@
 module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        console.log("je suis pret!")
-    }
-}
+  name: "ready",
+  once: true,
+  async execute(client) {
+    console.log("je suis pret!");
+  
+    const devGuild = await client.guilds.cache.get('1036617599250866187')
+    devGuild.commands.set(client.commands.map(cmd => cmd))
+
+  },
+};
---
+++ utils/handlers/CommandUtil.js
@@ -0,0 +1,15 @@
+const {promisify} = require('util')
+const {glob} = require('glob')
+const pGlob = promisify(glob)
+
+module.exports = async client => {
+    (await pGlob(`${process.cwd() }/commands/*/*.js`)).map(async cmdFile => {
+        const cmd = require(cmdFile)
+
+if (!cmd.name) return console.log(`--------\nCommande non-chargée: pas de nom et ou description\n Fichier -> ${cmdFile}\n--------`)
+
+        client.commands.set(cmd.name, cmd)
+        console.log(`Commande chargée: ${cmd.name}`)
+    })
+}
+
